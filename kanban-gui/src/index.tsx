641f91eca3543096afc4e0237d77fde1196948f3
IvanNSBS <insbs@cin.ufpe.br>
2021-08-25 21:43:50 -0300

    Adding styled components

--- kanban-gui/src/index.tsx
+++ kanban-gui/src/index.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 import Card from '../../data/cards/card'
+import styled from 'styled-components'
+
 
 const card:Card = new Card("Example Card Name")
+const RedHeader = styled.h2`
+    background-color: red;
+    color: whitesmoke;
+`
+
+const CardName: React.FC<{text:string}> = (props) => {
+    return(
+      <h2>Card Name: {props.text}</h2>  
+    );
+}
+
 
 ReactDOM.render(
     <h1>
-        Hello React22!
-        <h2>
-            Card Name: {card.name}
-        </h2>
+        <CardName text={card.name}></CardName>
     </h1>,
     document.getElementById('app-root'),
 )
