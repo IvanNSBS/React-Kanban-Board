48d3383846188dff77ececf713705ea4b439acb4
IvanNSBS <insbs@cin.ufpe.br>
2021-08-28 11:53:01 -0300

    Adding workspace to board data

--- data/board/board.ts
+++ data/board/board.ts
@@ -1,14 +1,19 @@
 import CardsList from "./cardList";
 
 export default class Board {
-    private _name: string = "";
-    private _cardsCollection: CardsList[] = [];
+    private _name: string;
+    private _workspace: string;
+    private _cardsCollection: CardsList[];
 
-    constructor(name: string){
+    constructor(name: string, workspace:string = ""){
         this._name = name;
         this._cardsCollection = [];
+        this._workspace = workspace;
     }
 
+    public get workspace() { return this._workspace; }
     public get name() { return this._name; }
     public get cardsCollection() { return this._cardsCollection; }
+
+    public set workspace(value: string) { this._workspace = value; }
 }
