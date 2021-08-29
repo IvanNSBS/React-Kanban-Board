234dfd30722cfb6a39b6537d9d972eded0e61e14
IvanNSBS <insbs@cin.ufpe.br>
2021-08-29 11:58:52 -0300

    Giving more configuration to FlexDiv style

--- kanban-gui/src/common/styles/FlexDiv.ts
+++ kanban-gui/src/common/styles/FlexDiv.ts
@@ -1,14 +1,25 @@
 import styled from "styled-components";
 
 interface FlexProperties{
-    justify: string;
-    align: string;
+    justify?: string;
+    alignContent?: string;
+    alignItems?: string;
+    direction?: string;
+    width?: string;
+    height?: string;
+    padding?: string;
+    margin?: string;
 }
 
 const FlexDiv = styled.div<FlexProperties>`
     display: flex;
+    flex-direction: ${(props) => props.direction };
     justify-content: ${(props) => props.justify };
-    align-items: ${(props) => props.align };
+    align-content: ${(props) => props.alignContent };
+    width: ${(props) => props.width };
+    height: ${(props) => props.height };
+    margin: ${(props) => props.margin };
+    padding: ${(props) => props.padding };
 `  
 
 export default FlexDiv;
