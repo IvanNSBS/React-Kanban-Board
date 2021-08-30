5aab98cfe7e94d9b0310f33a06289d55e3e3d443
IvanNSBS <insbs@cin.ufpe.br>
2021-08-30 20:48:23 -0300

    Renamed Folder to FolderBoardsDisplay on AccountBoards

--- kanban-gui/src/views/home/account boards/AccountBoards.tsx
+++ kanban-gui/src/views/home/account boards/AccountBoards.tsx
@@ -2,7 +2,7 @@ import User from '../../../../../data/account/user'
 import React, { useState } from "react";
 import { AiOutlineStar } from 'react-icons/ai'
 import * as styles from './AccountBoards.styles'
-import Folder from './FolderBoardsDisplay'
+import FolderBoardsDisplay from './FolderBoardsDisplay'
 import CreateBoardModal from './CreateBoardModal';
 import BoardsFolder from '../../../../../data/account/boardsFolder';
 
@@ -14,16 +14,16 @@ const UserBoards: React.FC<{account: User}> = function(props) {
     const folders = props.account.folders.map((folder, idx) => {
         return (
             // TODO: Add icon to folder data
-            <Folder boards={folder.boards} title={folder.name} 
+            <FolderBoardsDisplay boards={folder.boards} title={folder.name} 
                     icon={<AiOutlineStar/>} onClickCreate={() => setModalOpen(true)}/>
         )
     })
 
     return(
         <styles.AccountBoardContainer>
-            <Folder boards={props.account.starredBoards} 
+            <FolderBoardsDisplay boards={props.account.starredBoards} 
                     title="Quadros com Estrela" icon={<AiOutlineStar/>}>
-            </Folder>
+            </FolderBoardsDisplay>
             {folders}
             <CreateBoardModal setActive={setModalOpen} isOpen={modalOpen} folders={props.account.folders}/>
         </styles.AccountBoardContainer>
