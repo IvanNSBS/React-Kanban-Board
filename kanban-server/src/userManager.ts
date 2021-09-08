653a175382e677b93d63724546db93710ab2f1eb
IvanNSBS <insbs@cin.ufpe.br>
2021-09-08 20:47:34 -0300

    Bugfix not adding initial mockup data to user

--- kanban-server/src/userManager.ts
+++ kanban-server/src/userManager.ts
@@ -13,9 +13,9 @@ class UserManager {
         const board = new Board('Server Folder Board', serverFolder.name, 
         'https://s2.glbimg.com/rZddFXm-uylDO-055Zo0yJLzNAw=/0x0:2761x1840/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_e84042ef78cb4708aeebdf1c68c6cbd6/internal_photos/bs/2020/5/o/Fup5dESmmxeeVEdtuMTA/jongsun-lee-f-pszo-jee8-unsplash.jpg')
 
-        user.folders.push(serverFolder);
+        this._user.folders.push(serverFolder);
         serverFolder.boards.push(board);
-        user.starredBoards.push(board);
+        this._user.starredBoards.push(board);
     }
 
     public get user() { return this._user; }
