8b22a84c4be311728e3bcb793c182b66b209602a
IvanNSBS <insbs@cin.ufpe.br>
2021-09-08 20:45:43 -0300

    Creating a user manager to manage the session user

--- kanban-server/src/routes/homeRoute.ts
+++ kanban-server/src/routes/homeRoute.ts
@@ -1,69 +1,58 @@
-import User from '../../../data/account/user';
 import express, { Router } from 'express';
 import BoardsFolder from '../../../data/account/boardsFolder';
 import Board from '../../../data/board/board';
+import userManager from '../userManager';
 
-export default class HomeRouteController {
-    private _user: User;
-    private _route: Router;
+const homeRoute = express.Router();
 
-    constructor(user: User) {
-        this._user = user;
-        this._route = express.Router();
-        this.setupRoutes();
+homeRoute.get('/', function (req: express.Request, res: express.Response) {
+    res.send(JSON.stringify(userManager.user))
+});
 
+homeRoute.get('/folders', function (req: express.Request, res: express.Response) {
+    console.log(JSON.stringify(userManager.user.folders));
+    res.send( JSON.stringify(userManager.user.folders) );
+})
+
+homeRoute.get('/favorites', function (req: express.Request, res: express.Response) {
+    console.log(JSON.stringify(userManager.user.starredBoards));
+    res.send( JSON.stringify(userManager.user.starredBoards) );
+})
+
+homeRoute.post('/folders', function(req: express.Request, res: express.Response) {
+    const folder = <BoardsFolder>req.body;
+    if(folder.name === undefined || folder.name === ""){
+        res.status(406).send('Invalid Folder');
     }
 
-    public get route() { return this._route; }
+    userManager.user.folders = userManager.user.folders.concat(folder);
+    res.status(200).send(`Folder with name ${folder.name} created`)
+})
 
+homeRoute.post('/favorites', function(req: express.Request, res: express.Response) {
+    const favorites = <Board[]>req.body;
+    if(favorites === undefined)
+        res.status(406).send('Invalid Folder');
 
-    private setupRoutes()
+    userManager.user.starredBoards = favorites;
+    res.status(200).send(`Updated Starred Boards`)
+})
+
+homeRoute.post('/boards', function(req: express.Request, res: express.Response) {
+    const folderIdx: number = req.body.folderIdx;
+    const folderName: string = req.body.folderName;
+    const name: string = req.body.name;
+    const bgImgUrl: string | undefined = req.body.bgImgUrl;
+
+    if( folderIdx < 0 || folderIdx >= userManager.user.folders.length || 
+        name === "" || name === undefined || folderName === "" || folderName === undefined)
     {
-        let that = this;
-        this._route.get('/folders', function (req: express.Request, res: express.Response) {
-            console.log(JSON.stringify(that._user.folders));
-            res.send( JSON.stringify(that._user.folders) );
-        })
-
-        this._route.get('/favorites', function (req: express.Request, res: express.Response) {
-            console.log(JSON.stringify(that._user.starredBoards));
-            res.send( JSON.stringify(that._user.starredBoards) );
-        })
-
-        this._route.post('/folders', function(req: express.Request, res: express.Response) {
-            const folder = <BoardsFolder>req.body;
-            if(folder.name === undefined || folder.name === ""){
-                res.status(406).send('Invalid Folder');
-            }
-
-            that._user.folders = that._user.folders.concat(folder);
-            res.status(200).send(`Folder with name ${folder.name} created`)
-        })
-
-        this._route.post('/favorites', function(req: express.Request, res: express.Response) {
-            const favorites = <Board[]>req.body;
-            if(favorites === undefined)
-                res.status(406).send('Invalid Folder');
-
-            that._user.starredBoards = favorites;
-            res.status(200).send(`Updated Starred Boards`)
-        })
-
-        this._route.post('/boards', function(req: express.Request, res: express.Response) {
-            const folderIdx: number = req.body.folderIdx;
-            const folderName: string = req.body.folderName;
-            const name: string = req.body.name;
-            const bgImgUrl: string | undefined = req.body.bgImgUrl;
-
-            if( folderIdx < 0 || folderIdx >= that._user.folders.length || 
-                name === "" || name === undefined || folderName === "" || folderName === undefined)
-            {
-                res.status(406).send('Invalid parameters for boards');
-            }
-            else{
-                that._user.folders[folderIdx].boards.push(new Board(name, folderName, bgImgUrl));
-                res.status(200).send(`Board created for folder ${folderName} Created`);
-            }
-        })
+        res.status(406).send('Invalid parameters for boards');
     }
-}
+    else{
+        userManager.user.folders[folderIdx].boards.push(new Board(name, folderName, bgImgUrl));
+        res.status(200).send(`Board created for folder ${folderName} Created`);
+    }
+})
+
+export default homeRoute;
--- kanban-server/src/server.ts
+++ kanban-server/src/server.ts
@@ -1,9 +1,7 @@
-import User from '../../data/account/user';
 import express, { Request, Response } from 'express';
 import cors from 'cors'
-import BoardsFolder from '../../data/account/boardsFolder';
-import Board from '../../data/board/board';
-import HomeRouteController from './routes/homeRoute';
+import homeRoute from './routes/homeRoute';
+import userManager from './userManager';
 
 const app = express();
 const PORT = 8000;
@@ -12,20 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}))
 
-const user: User = new User('Server User');
-const serverFolder = new BoardsFolder('Server Folder', undefined);
-const board = new Board('Server Folder Board', serverFolder.name, 'https://s2.glbimg.com/rZddFXm-uylDO-055Zo0yJLzNAw=/0x0:2761x1840/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_e84042ef78cb4708aeebdf1c68c6cbd6/internal_photos/bs/2020/5/o/Fup5dESmmxeeVEdtuMTA/jongsun-lee-f-pszo-jee8-unsplash.jpg')
-
-user.folders.push(serverFolder);
-serverFolder.boards.push(board);
-user.starredBoards.push(board);
-
-const homeRouteController = new HomeRouteController(user);
-
-app.use('/', homeRouteController.route);
-app.get('/', (req: Request, res: Response) => {
-    res.send(JSON.stringify(user))
-});
+app.use('/', homeRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running at https://localhost:${PORT}`);
