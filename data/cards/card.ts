734d38b791bb62ab68f5a1fb2bd3c7e432d924b0
IvanNSBS <insbs@cin.ufpe.br>
2021-09-17 23:37:10 -0300

    Making card serializable

--- data/cards/card.ts
+++ data/cards/card.ts
@@ -2,17 +2,15 @@ import Tag from './tag';
 import Checklist from './checklist';
 
 export default class Card {
-    private _name: string;
-    private _description: string;
-    private _associatedTags: Tag[];
-    private _checklists: Checklist[];
+    public name: string;
+    public description: string;
+    public associatedTags: Tag[];
+    public checklists: Checklist[];
 
     constructor(name: string){
-        this._name = name;
-        this._description = "";
-        this._associatedTags = [];
-        this._checklists = [];
+        this.name = name;
+        this.description = "";
+        this.associatedTags = [];
+        this.checklists = [];
     }
-
-    public get name() { return this._name; }
 }
